2ebeb67cfa5d82027d3b9899d84650069a3f2f97
rfsaldanha <rfsaldanha@gmail.com>
2026-01-12 11:34:29 -0300

    Read csv file

--- R/read_users.R
+++ R/read_users.R
@@ -1,27 +1,4 @@
 #' @export
-read_users <- function() {
-  tibble::tibble(
-    user_id = c(
-      "zVNzVYAAAAAJ",
-      "PZWm2wYAAAAJ",
-      "UiXjYXsAAAAJ",
-      "vn-A07gAAAAJ",
-      "UJper5AAAAAJ",
-      "vFoixy4AAAAJ",
-      "9kkHqvoAAAAJ",
-      "A3La3osAAAAJ",
-      "aR0BDGwAAAAJ"
-    ),
-    name = c(
-      "Christovam Barcellos",
-      "Diego Xavier",
-      "Raphael Saldanha",
-      "Renata Gracie",
-      "Ricardo Oliveira",
-      "Marcel Pedroso",
-      "Raquel De Boni",
-      "Paulo Borges",
-      "Cristiano Boccolini"
-    )
-  )
+read_users <- function(file) {
+  readr::read_csv2(file = file)
 }
